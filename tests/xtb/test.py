e40dd176915e24b48fdab0226399f3e20dc7758e
Gunnar Schmitz <gunnar.schmitz@rub.de>
2020-10-03 09:34:23 +0200

    Added xTB test

---
+++ tests/xtb/test.py
@@ -0,0 +1,41 @@
+
+import numpy as np
+
+import pylmps
+import molsys
+
+
+mol = molsys.mol.from_file("geom.xyz")
+
+pl = pylmps.pylmps("opt")
+
+# instantiate the external potential expot_xtb
+
+# GFN1 calculatuion (muted verbose=0, set to 2 for full output)
+ep = pylmps.expot_xtb(mol,1,verbose=0,maxiter=1000)
+
+# we need to register the objects callback as a global function
+callback = ep.callback
+
+# now add the expot object together with the name of the global callback
+pl.add_external_potential(ep, "callback")
+
+# setup xTB
+pl.setup(local=True, mol = mol, ff="xTB")
+
+final_energy = pl.MIN(0.01)
+
+
+#
+# Do the test
+#
+ref_energies = {
+            'xtb': -27399.693901383605
+}
+
+
+energies = pl.get_energy_contribs()
+
+for e in ref_energies.keys():
+   assert abs(ref_energies[e]-energies[e])<1.0e-6
+
